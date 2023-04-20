34b4fa9fa962ec46e8eff3dd21155118171e5f42
fi0n4m <111779860+fi0n4m@users.noreply.github.com>
2023-04-20 11:42:48 +0800

    Create BookRentalSystem.java

---
+++ Basic OOP Book Rental/BookRentalSystem.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class BookRentalSystem {
+
+	public static void main(String[] args) {
+
+		ArrayList<Book> books = new ArrayList<>();
+
+		Book book1 = new Book();
+
+		book1.name = " System Analysis and Design ";
+		book1.author = "Gary B. Shelly";
+		book1.copy = 2;
+
+		Book book2 = new Book();
+
+		book2.name = " Android Application ";
+		book2.author = " Corine Hoisington ";
+		book2.copy = 3;
+
+		Book book3 = new Book();
+
+		book3.name = " Programming Concepts and Logic Formulation ";
+		book3.author = " Rosauro E. Manuel ";
+		book3.copy = 4;
+
+		books.add(book1);
+
+		books.add(book2);
+
+		books.add(book3);
+
+		System.out.println("ELECTRONIC BOOK RENTAL SYSTEM");
+		System.out.println("******************************");
+		System.out.println("Choose a number to rent a book");
+
+		for (int i = 0; i < books.size(); i++) {
+			System.out.print(i);
+
+			System.out.print(" " + books.get(i).getName() + ", " + books.get(i).getAuthor());
+
+			System.out.println();
+		
+		
+		}
+
+		boolean end = false;
+
+		while (!end) {
+			
+			try {
+				Scanner scan = new Scanner(System.in);
+		
+				
+				System.out.print("\nPick one number: ");
+				int choice = scan.nextInt();
+
+				
+				Book book = books.get(choice);
+				int copy = book.getCopy();
+				
+				
+				if (copy > 0) {
+					System.out.println("You've Rented : " + books.get(choice).getName());
+					book.setCopy(copy -= 1);
+				} else {
+					System.out.println("No copies available");
+				
+				
+				}
+
+				System.out.print("\nDo you want to Rent Again? [Y/N]");
+				String ask = scan.next();
+				
+				
+				
+				if (ask.equalsIgnoreCase("N")) {
+					end = true;
+				}
+
+
+			} catch (InputMismatchException e) {
+				System.out.println("Error! Input must be a number only");
+			} catch (IndexOutOfBoundsException e) {
+				System.out.println("Error: Input is not found in the choices");
+			}
+
+
+
+		}
+	}
+}
